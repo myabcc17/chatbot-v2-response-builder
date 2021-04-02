003e29befb10c5b5c1816aaae01b108ae7cfa804
WooHyeok Jeon <myabcc17@naver.com>
2021-04-02 17:45:57 +0900

    Add Enum Button.Action

--- src/main/java/com/github/myabcc17/template/common/Button.java
+++ src/main/java/com/github/myabcc17/template/common/Button.java
@@ -23,7 +23,22 @@ public class Button {
     private static final int MAX_LABEL = 14;
     private static final List<String> actionCadidates = List.of("webLink", "message", "block", "phone", "share", "operator");
 
-    private Button(String label, String action, String webLinkUrl, String messageText,
+    public enum Action {
+        WEBLINK("webLink"),
+        MESSAGE("message"),
+        BLOCK("block"),
+        PHONE("phone"),
+        SHARE("share"),
+        OPERATOR("operator");
+
+        private String value;
+
+        Action(String value) {
+            this.value = value;
+        }
+    }
+
+    private Button(String label, Action action, String webLinkUrl, String messageText,
             String phoneNumber, String blockId,
             Map<String, Object> extra) {
         Objects.requireNonNull(label);
@@ -31,27 +46,23 @@ public class Button {
 
         StringValidator.validateLength(label, MAX_LABEL);
 
-        if (!actionCadidates.contains(action)) {
-            throw new RuntimeException(String.format("action은 해당 값 중 하나만 가능합니다. %s", actionCadidates));
-        }
-
         switch (action) {
-            case "webLink" -> {
+            case WEBLINK -> {
                 Objects.requireNonNull(webLinkUrl);
                 if (!UrlUtils.isValidUrl(webLinkUrl)) {
                     throw new InvalidUrlException();
                 }
             }
-            case "message" -> Objects.requireNonNull(messageText);
-            case "phone" -> Objects.requireNonNull(phoneNumber);
-            case "block" -> {
+            case MESSAGE -> Objects.requireNonNull(messageText);
+            case PHONE -> Objects.requireNonNull(phoneNumber);
+            case BLOCK -> {
                 Objects.requireNonNull(blockId);
                 Objects.requireNonNull(messageText);
             }
         }
 
         this.label = label;
-        this.action = action;
+        this.action = action.value;
         this.webLinkUrl = webLinkUrl;
         this.messageText = messageText;
         this.phoneNumber = phoneNumber;
@@ -59,26 +70,26 @@ public class Button {
         this.extra = extra;
     }
 
-    public static Button of(String label, String action, String webLinkUrl, String messageText,
+    public static Button of(String label, Action action, String webLinkUrl, String messageText,
             String phoneNumber, String blockId,
             Map<String, Object> extra) {
         return new Button(label, action, webLinkUrl, messageText, phoneNumber, blockId, extra);
     }
 
-    public static ButtonBuilder builder(String label, String action) {
+    public static ButtonBuilder builder(String label, Action action) {
         return new ButtonBuilder(label, action);
     }
 
     public static class ButtonBuilder {
         private final String label;
-        private final String action;
+        private final Action action;
         private String webLinkUrl;
         private String messageText;
         private String phoneNumber;
         private String blockId;
         private Map<String, Object> extra;
 
-        public ButtonBuilder(String label, String action) {
+        public ButtonBuilder(String label, Action action) {
             this.label = label;
             this.action = action;
         }
--- src/test/java/CreateTest.java
+++ src/test/java/CreateTest.java
@@ -1,38 +1,15 @@
 import com.github.myabcc17.SkillResponseV2Builder;
-import com.github.myabcc17.request.SkillPayload;
 import com.github.myabcc17.template.ContextValue;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import com.github.myabcc17.template.component.SimpleImage;
 import com.github.myabcc17.template.component.SimpleText;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Map;
+import org.junit.jupiter.api.Test;
 
 public class CreateTest {
     SkillResponseV2Builder skillResponseV2Builder = new SkillResponseV2Builder();
-/*
-    @AfterEach
-    void print() {
-        System.out.println("=============================================");
-        System.out.println(gson.toJson(skillResponseV2Builder.build()));
-        System.out.println("=============================================");
-    }*/
 
     @Test
     void testSimpleText() {
         skillResponseV2Builder.addComponent(SimpleText.of("simpleText of"));
         skillResponseV2Builder.addComponent(SimpleText.builder("simpleText").build());
-    }
-
-    @Test
-    void testSimpleImage() {
-        skillResponseV2Builder.addComponent(SimpleImage.of("awef@awfew", "표시할 수 없음"));
-        skillResponseV2Builder.addComponent(SimpleImage.builder("awef@awfew", "대체 문구").build());
     }
 
     @Test
